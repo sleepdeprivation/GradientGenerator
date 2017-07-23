607ccdebb43f3612fe39af209b71a369d7c04109
Christian Burke <cburke91@live.com>
2017-07-22 18:48:19 -0700

    Finished gradient MVP and decided againts adding flux etc

--- src/App.js
+++ src/App.js
@@ -2,32 +2,31 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-var gradStyle = {
-    background: 'linear-gradient(to right, red, yellow)',
-    height:     '400px'
-};
+//https://www.paulirish.com/2009/random-hex-color-code-snippets/
+var randomHex = function(){ return '#'+Math.floor(Math.random()*16777215).toString(16);};
 
 class GradientInput extends Component {
 
-    constructor(props){
-        super(props);
-        this.state = {color : "green"};
-    }
+  constructor(props){
+      super(props);
+  }
 
-    setColor(){
-        this.setState({color : "blue"});
-    }
+  setColor(){
+      console.log("settingColor")
+      this.props.updateColor(randomHex());
+  }
 
-    colorChanged(event){
-        this.setState({color: event.target.value});
-    }
+  colorChanged(event){
+      this.props.updateColor(event.target.value);
+  }
 
   render() {
+      console.log("rendering");
     return (
         <div>
             <input  className="GradientInput"
-                    value={this.state.color}
-                    onChange={this.colorChanged} ></input>
+                    value={this.props.color}
+                    onChange={this.colorChanged.bind(this)} ></input>
                 <button onClick={this.setColor.bind(this)}>
                     REFRESH</button>
         </div>
@@ -35,33 +34,64 @@ class GradientInput extends Component {
   }
 }
 
-
-    GradientInput.defaultProps = {
-        color: 'blue'
-    }
-
-
-
-
+var gradStyle = function(left, right){
+  return {
+    background: 'linear-gradient(to right, '+left+', '+right+')',
+    height:     '400px'
+  };
+};
 class Gradient extends Component {
   render() {
+    console.log("rendering css gradient");
     return (
-      <div className="GradientBox" style={gradStyle}>
+      <div  className="GradientBox"
+            style={gradStyle(this.props.color1, this.props.color2)} >
       </div>
     );
   }
 }
 
 class App extends Component {
+
+  constructor(props){
+      super(props);
+      this.state = (
+          {
+            'color1' : this.props.color1,
+            'color2' : this.props.color2
+          });
+  }
+
+  updateState(which){
+      return function(arg){
+          console.log("updated", which, arg);
+          var obj = {};
+          obj[which] = arg;
+          this.setState( obj );
+          console.log(this.state);
+      }
+  }
+
   render() {
     return (
       <div className="App">
-          <GradientInput />
-          <GradientInput />
-          <Gradient/>
+          <GradientInput
+              color={this.state.color1}
+              updateColor={this.updateState('color1').bind(this)}
+          />
+          <GradientInput
+              color={this.state.color2}
+              updateColor={this.updateState('color2').bind(this)}
+          />
+          <Gradient color1={this.state.color1} color2={this.state.color2}/>
       </div>
     );
   }
+}
+
+App.defaultProps = {
+    color1: randomHex(),
+    color2: randomHex()
 }
 
 
--- src/reducers/gradientstore.js
+++
@@ -1,9 +0,0 @@
-import {Dispatcher} from 'flux';
-
-export default new Dispatcher();
-
-const ActionTypes = {
-  ADD_TODO: 'ADD_TODO',
-};
-
-export default ActionTypes;
